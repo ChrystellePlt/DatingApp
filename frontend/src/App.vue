29c5c0a365c706ac08eee5be0c488404cc8398a0
ChrystellePoulet <chrystelle.poulet@adfab.fr>
2019-03-28 18:03:45 +0100

    Integrating Auth0 with the Vue.js front-end

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,74 @@
+/* eslint-disable */
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="!authenticated"
+      @click="login()">
+      Log In
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="privateMessage()">
+      Call Private
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="logout()">
+      Log Out
+    </button>
+    {{ message }}
+    <br>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-};
-</script>
+import axios from 'axios';
+import AuthService from './auth/AuthService';
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+const API_URL = 'http://localhost:8000';
+const auth = new AuthService();
+
+export default {
+  name: 'app',
+  data() {
+    this.handleAuthentication();
+    this.authenticated = false;
+
+    auth.authNotifier.on('authChange', (authState) => {
+      this.authenticated = authState.authenticated;
+    });
+
+    return {
+      authenticated: false,
+      message: '',
+    };
+  },
+
+  methods: {
+    // this method calls the AuthService login() method
+    login() {
+      auth.login();
+    },
+    handleAuthentication() {
+      auth.handleAuthentication();
+    },
+    logout() {
+      auth.logout();
+    },
+    privateMessage() {
+      const url = `${API_URL}/api/private/`;
+      return axios.get(url, { headers: { Authorization: `Bearer ${AuthService.getAuthToken()}` } }).then((response) => {
+        console.log(response.data);
+        this.message = response.data || '';
+      });
+    },
+  },
+};
+
+</script>
